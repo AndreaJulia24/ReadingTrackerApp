06581ad95bb5fcf3452cbcad356e302ee2dac3bb
AndreaJulia24 <czellecza@yahoo.com>
2025-11-02 14:42:47 +0200

    Add files via upload

---
+++ BookTrackerApp/src/main/java/com/mycompany/booktrackerapp/BookTrackerApp.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.booktrackerapp;
+
+/**
+ *
+ * @author Andi
+ */
+public class BookTrackerApp {
+    public static void main(String[] args) {
+        
+        BookTrackerFrame myFrame = new BookTrackerFrame();
+        myFrame.setVisible(true);
+    }
+    
+}
---
+++ BookTrackerApp/src/main/java/com/mycompany/booktrackerapp/User.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.booktrackerapp;
+
+/**
+ *
+ * @author Andi
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private String username;
+    private String email;
+    private List<ReadingItem>readingList; //a User olvasasi bejegyzeseinek listaja
+    
+    public User(){
+        this.readingList=new ArrayList<>();
+    }
+
+    public User(String MY_USERNAME, String email) {
+        this.username = MY_USERNAME;
+        this.email = email;
+        this.readingList = new ArrayList<>();
+    }
+    
+
+    public String getusername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List<ReadingItem> getReadingList() {
+        return readingList;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    //hozzaad egy uj olvasasi bejegyzest a user listajahoz
+    public void addReadingItem(ReadingItem item){
+        this.readingList.add(item);
+    }
+    
+    //listazza az olvasi bejegyzeseit a Usernek (konzolos verzió, amit a GUI nem használ)
+    public void listReadingItem(User user){
+        
+        if(user==null){
+        return;
+        }
+        List<ReadingItem> readingList = user.getReadingList();
+        
+        if(readingList.isEmpty()){
+            System.out.println(this.username + "the user has no recorded reading yet.");
+        }
+        System.out.println(this.username + "reading list: ");
+        int lineNum=1;
+        for(ReadingItem item : readingList){
+            System.out.println(lineNum + "." + item.toString());
+            lineNum++;
+        }
+    }
+    
+    public int CalcTotalReadPages(){
+        int totalPage=0;
+        for(ReadingItem item : readingList){
+            if(item.getStatus()==ReadingItem.Status.READ){
+                totalPage += item.getBook().getPage();
+            }
+        }
+        return totalPage;
+    }
+}
